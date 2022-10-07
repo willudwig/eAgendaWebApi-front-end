fa9bf9d903a29ae49291dca4968ddeaf28f6c0cb
William Ludwig <acesso.william@gmail.com>
2022-10-07 14:49:48 -0300

    login.guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegistroComponent } from './auth/registro/registro.component';
+import { AuthGuard } from './auth/services/auth.guard';
 
 const routes: Routes = [
   {
@@ -20,7 +21,9 @@ const routes: Routes = [
   {
     path: 'dashboard',
     loadChildren: () => import('./dashboard/dashboard.module')
-    .then(m => m.DashboardModule)
+    .then(m => m.DashboardModule),
+
+    canActivate: [AuthGuard]
   }
 ];
 
